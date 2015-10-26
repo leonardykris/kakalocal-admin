34a36d168b56dae993ae9c4133b153529d0bf905
Leonardy Kristianto <alendory@gmail.com>
2015-10-27 05:04:03 +0800

    revamped udpate panel

--- client/views/guides.js
+++ client/views/guides.js
@@ -7,10 +7,10 @@ Template.guides.helpers({
 			fields: [
 				{ key: 'firstName', label: 'First Name' },
 				{ key: 'lastName', label: 'Last Name' },
-				{ key: 'phone', label: 'Contact No.', hidden: true },
+				{ key: 'phone', label: 'Contact No.' },
 				{ key: 'email', label: 'Email' },
 				{ key: 'country', label: 'Country' },
-				{ key: 'city', label: 'City', hidden: true },
+				{ key: 'city', label: 'City' },
 				{ key: 'referrer', label: 'Referrer', hidden: true },
 				{ key: 'remarks', label: 'Remarks', hidden: true },
 				{
@@ -23,34 +23,35 @@ Template.guides.helpers({
 			],
 			showRowCount: true,
 			showColumnToggles: true,
-			class: 'table table-bordered table-hover col-sm-12',
+			class: 'table table-bordered table-hover col-sm-12 tr-pointer ',
 		};
 	},
-	selectedGuideDoc: function() {
-		return Guides.findOne(Session.get("selectedGuideId"));
-	},
-	isSelectedGuide: function() {
-		return Session.equals("selectedGuideId", this._id);
-	},
-	formType: function() {
-		if (Session.get("selectedGuideId")) {
-			return "update";
-		}
-		else {
-			return "insert";
-		}
-	},
+	// selectedGuideDoc: function() {
+	// 	return Guides.findOne(Session.get("selectedGuideId"));
+	// },
+	// isSelectedGuide: function() {
+	// 	return Session.equals("selectedGuideId", this._id);
+	// },
+	// formType: function() {
+	// 	if (Session.get("selectedGuideId")) {
+	// 		return "update";
+	// 	}
+	// 	else {
+	// 		return "insert";
+	// 	}
+	// },
 });
 
 Template.guides.events({
 	'click tr': function(){
 		// console.log(this._id);
-		Session.set("selectedGuideId", this._id);
+		// Session.set("selectedGuideId", this._id);
+		Router.go('guideDetails', { _id: this._id });
 	},
-	'click #resetForm': function(){
-		Session.set("selectedGuideId", null);
-		AutoForm.resetForm("guideForm");
-	}
+	// 'click #resetForm': function(){
+	// 	Session.set("selectedGuideId", null);
+	// 	AutoForm.resetForm("guideForm");
+	// }
 });
 
 
