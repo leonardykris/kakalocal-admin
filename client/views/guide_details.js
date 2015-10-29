c982637a48e4f7ad13812a525509fb356e2fc43f
Leonardy Kristianto <alendory@gmail.com>
2015-10-30 06:05:38 +0800

    added admin-publish, added more how to

--- client/views/guide_details.js
+++ client/views/guide_details.js
@@ -1,10 +1,13 @@
 Template.guideDetails.helpers({
 	selectedGuideDoc: function() {
-		return this;
+    // Check whether the page has a document loaded with it, passed by iron router
+    if (this) { return this; }
+    else { return null; }
 	},
 });
 
 Template.guideDetails.onRendered(function() {
+  // Change the colors of the + and - buttons
 	$('.autoform-add-item').removeClass('btn-primary').addClass('btn-default');
 	$('.autoform-remove-item').removeClass('btn-primary').addClass('btn-default');
 });
@@ -12,5 +15,21 @@ Template.guideDetails.onRendered(function() {
 Template.guideDetails.events({
 	'click .autoform-add-item': function () {
 		$('.autoform-remove-item').removeClass('btn-primary').addClass('btn-default');
+	},
+	'click #publishButton': function() {
+    Meteor.call('publishGuide', this, true,
+      function (error, result) {
+        if (!error) { console.log(result); }
+        else { console.log(error); }
+      }
+    );
+  },
+  'click #unPublishButton': function() {
+    Meteor.call('publishGuide', this, false,
+      function (error, result) {
+        if (!error) { console.log(result); }
+        else { console.log(error); }
+      }
+    );
 	},
 });
--- server/methods.js
+++ server/methods.js
@@ -2,7 +2,7 @@ Meteor.methods({
 	submitForm: function(doc) {
 		console.log("Client has called submitForm method.")
 		if (doc) {
-			// console.log(doc);
+			// Temporarily halted
 			// check(doc, {
 			// 	_id: String,
 			// 	type: String,
@@ -13,40 +13,54 @@ Meteor.methods({
 			// });
 			if (doc._id) {
 				if (Guides.findOne({ _id: doc._id })) {
-					console.log(doc._id + " already existed! It was last updated on " + doc.updatedAt + ".");
+					console.log(doc._id + " exists! It was last updated on " + doc.updatedAt + ".");
 					doc.updatedAt = moment().format();
 
 					Guides.update({ _id: doc._id }, { $set: doc }, function(error, result){
-						if (error) {
-							console.log(error);
-						}
-						else {
-							console.log(result + " document(s) have been updated.");
-						}
+						if (!error) { console.log(result + " document(s) have been updated."); return true; }
+						else { console.log(error); }
 					});
-				}
-				else {
-					console.log("Document with _id " + doc._id + " was not found.");
-				}
-			}
-			else {
+				} else { console.log("Document with _id " + doc._id + " was not found."); }
+			} else {
 				// Assign _id here, because if it's in client it will keep spawning the _id even if selected user already have it
 				doc._id = Random.id();
 				doc.updatedAt = doc.createdAt = moment().format();
 
 				Guides.insert(doc, function(error, result){
-					if (error) {
-						console.log(error);
-					}
-					else {
-						console.log(result + " has been inserted.");
-					}
+					if (!error) { console.log(result + " has been inserted."); return true; }
+					else { console.log(error); }
 				});
 			}
-		}
-		else {
-			console.log("Document does not exist.");
-		}
-
+		} else { console.log("Document does not exist."); }
 	},
+	// This publishGuide method can both publish and unpublish a guide based on the params passed onto it
+	publishGuide: function(doc, params) {
+		console.log("Client has called publishGuide method.");
+		var params = params;
+
+		// We do triple checking here before get to update the collection
+		// 1. Whether the document is properly passed
+		// 2. Whether the document is a valid document with an _id, why?
+		// 	2.1 Because an invalid document does not have an _id
+		//	2.2 We only spawn an _id in the server once we have added a user
+		// 3. Whether the document in the database that match the passed _id exists
+		// Call me paranoid, but since I'm not doing check(doc, {params}) here I need all this
+		if (doc) {
+			if (doc._id) {
+				if (Guides.findOne({ _id: doc._id })) {
+					console.log(doc._id + " exists! It was last updated on " + doc.updatedAt + ".");
+					doc.updatedAt = moment().format();
+
+					Guides.update(
+						{ _id: doc._id },
+						{ $set: { isPublished: params }},
+						function(error, result){
+							if (!error) { console.log(result + " document(s) have been updated."); return true; }
+							else { console.log(error); }
+						}
+					);
+				} else { console.log("Document with _id " + doc._id + " was not found."); }
+			} else { console.log("Invalid document."); }
+		} else { console.log("Document does not exist."); }
+	}
 });
